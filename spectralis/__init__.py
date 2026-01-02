1fb8a61002b8f90fc3a338a5105b6fff10c60f56
JustinGISP <justingisp@gmail.com>
2026-01-02 16:30:16 -0500

    Update __init__.py

--- spectralis/__init__.py
+++ spectralis/__init__.py
@@ -8,6 +8,6 @@ __version__ = "0.0.1"
 __author__ = "Telluris Labs"
 __email__ = "info@tellurislabs.io"
 __license__ = "MIT"
-__description__ = "Custom tools for atmospheric and energy flux data processing and analysis"
+__description__ = "Custom tools for remote sensing / earth observation data"
 
 # Import selected functions and classes to top-level namespace
